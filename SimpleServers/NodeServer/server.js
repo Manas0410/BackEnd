c6992ffe9a4c2d4eaaae5a93760a59a1fbeeff61
manas0410 <manasshrivastava0410@gmail.com>
2024-03-18 23:31:12 +0530

    node apis added

--- SimpleServers/NodeServer/server.js
+++ SimpleServers/NodeServer/server.js
@@ -1,11 +1,39 @@
-const app = require("http");
+const http = require("http");
+const url = require("url");
 const port = 8002;
 
-const server = app.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end(`node server running`);
+const server = http.createServer(async (req, res) => {
+  try {
+    const receivedUrl = url.parse(req.url, true);
+    console.log(receivedUrl, "r");
+    if (receivedUrl.pathname === "/greetings") {
+      const queryName = receivedUrl.query.name;
+      if (queryName) {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`Hello ${queryName}`);
+      } else {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`Hello there`);
+      }
+    } else if (receivedUrl.pathname === "/products") {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.text();
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(`Node server running`);
+    }
+  } catch (err) {
+    console.log(err);
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end(`Internal Server Error`);
+  }
 });
 
 server.listen(port, () => {
-  console.log(`node server running on port ${port}`);
+  console.log(`Node server running on port ${port}`);
 });
