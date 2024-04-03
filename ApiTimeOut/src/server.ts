1d4c92d28f0f4263ec5f49bd3ff73fc7e5b8029e
manas0410 <manasshrivastava0410@gmail.com>
2024-04-03 19:03:02 +0530

    new changes

--- ApiTimeOut/src/server.ts
+++ ApiTimeOut/src/server.ts
@@ -2,16 +2,24 @@ import express from "express";
 import route from "./Routes/route";
 import ApiTimeOutMiddleWare from "./Middleware/middleware.apiTimeOut";
 
-const app = express();
-const port = 3000;
+const server: () => void = () => {
+  try {
+    const app = express();
+    const port = 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("api is working");
-});
+    app.get("/", (req, res) => {
+      res.status(200).send("api is working");
+    });
 
-app.use(ApiTimeOutMiddleWare(100));
-app.use("/timeOutApi", route);
+    app.use(ApiTimeOutMiddleWare(100));
+    app.use("/timeOutApi", route);
 
-app.listen(port, () => {
-  console.log("app is running on http://localhost:3000");
-});
+    app.listen(port, () => {
+      console.log("app is running on http://localhost:3000");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+server();
